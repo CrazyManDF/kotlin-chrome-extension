9efda00ff1c6bd154bf6c4d10cbe54a33b3b9d90
darren <darrenfoung@gmail.com>
2024-06-21 16:20:50 +0800

    增加 管理标签页，分组

--- popup/src/jsMain/kotlin/popup.kt
+++ popup/src/jsMain/kotlin/popup.kt
@@ -128,6 +128,82 @@ private fun sendMessageView() {
     }) {
         Text("Send message")
     }
+    Text(result)
+}
+
+
+private fun sendMessage(onResponse: (Message) -> Unit) {
+    val message = Message(action = "POPUP_CLICK", message = "Message from popup")
+    chrome.runtime.sendMessage(
+        message = Json.encodeToString(message),
+        responseCallback = { response ->
+            val responseMessage = Json.decodeFromString<Message>(response.toString())
+            onResponse(responseMessage) // Message from background to popup
+        }
+    )
+}
+
+private fun queryTabs(callback: (Array<Tab>) -> Unit) {
+    chrome.tabs.query(QueryInfo {
+        url = arrayOf(
+            "https://developer.chrome.com/docs/webstore/*",
+            "https://developer.chrome.com/docs/extensions/*"
+        )
+    }) { tabs ->
+        callback(tabs)
+    }
+}
+
+private fun fillTabsData() {
+    queryTabs { tabs ->
+        tabs.sortWith(compareBy({it.title}))
+        // 标签数据
+        val template = document.getElementById("li_template")
+        val elements = hashSetOf<Element>()
+        for (tab in tabs) {
+
+            println("标签${tab.id}：${tab.title}")
+            println("标签${tab.id}：${tab.url}")
+            val pathName = URL(tab.url!!).pathname
+
+            val title = tab.title?.split("-")?.get(0)?.trim()
+            val pathname = pathName.slice("/docs".length..<pathName.length)
+
+            val element = template?.firstElementChild?.cloneNode(true) as Element
+            element.querySelector(".title")?.textContent = title
+            element.querySelector(".pathname")?.textContent = pathname
+            element.querySelector("a")?.addEventListener("click", EventListener {
+                chrome.tabs.update(tab.id, UpdateProperties { active = true }, {})
+                chrome.windows.update(tab.windowId, UpdateInfo { focused = true }, {})
+            })
+            elements.add(element)
+        }
+        document.querySelector("ul")?.append(*(elements.toTypedArray()))
+    }
+}
+
+
+/**
+ * 测试发送消息布局
+ */
+@Composable
+private fun sendMessageView() {
+
+    var result by remember { mutableStateOf("") }
+
+    H3 {
+        Text("Hello Extensions")
+    }
+
+    Button({
+        onClick {
+            sendMessage {
+                result = "Message result: ${it.message}"
+            }
+        }
+    }) {
+        Text("Send message")
+    }
 
     Text(result)
 }
